f19b57e641af83cbaf81bf03d4c9c88006ce7a60
Pallab Ganguly <pallabkumarganguly@gmail.com>
2018-09-01 23:09:43 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8080;
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+})
