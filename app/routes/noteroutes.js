0422b613d754ac8176772b44a6f8a3243d8c9c48
Pallab Ganguly <pallabkumarganguly@gmail.com>
2018-09-02 18:44:30 +0530

    added config for db

--- app/routes/noteroutes.js
+++ app/routes/noteroutes.js
@@ -2,5 +2,11 @@ module.exports = function(app, db) {
     // just one service route
     app.post('/notes', (req, res) => {
         res.send("Hello, world!")
+        console.log(req.body)
+    });
+
+    app.get('/', (req, res) => {
+        res.json({'message': 'Welcome to the home page of the application!'})
+        console.log('In GET')
     });
 };
